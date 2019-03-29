020405be4eb5cf3e5009ddc821ddd92dd60378e9
sdkdeepak1612 <47604950+sdkdeepak1612@users.noreply.github.com>
2019-03-29 21:01:42 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
-from datetime import  datetime
-from modules import agee,autoage,infor, newmarks
-import  xlrd
+from datetime import datetime
+from modules import agee, autoage, infor, newmarks
+import xlrd
 from xlutils.copy import copy
 
 print('\t\t\t\t\t\tWELCOME TO STUDENT DATABASE MANAGEMENT')
@@ -25,129 +25,142 @@ def iput():
         for i in range(rsheet.nrows):
 
             user_no = rsheet.cell_value(i, 0)  # to get the username from the exl sheet
-            exl_user = (str(user_no)[:4])  # The user number from the exl is in float form (4001.0) so we take the first 4 values
+            exl_user = (str(user_no)[:4])  # The user number from the exl is in float form (4001.0) so we take the first  4 values
             exl_pwd = str(rsheet.cell_value(i, 1))  # the password got from the excel
             main = i  # it is important to locate the  student or student data
+            def pro(a, b):
+                    user_name = a
+                    pwd = b
 
-            if exl_user == user_name and pwd == exl_pwd:
+                    if exl_user == user_name and pwd == exl_pwd:
+                        found = True
 
-                print('\n------------------------------------------------------------------------------------------\n')
-                print('\t\t\t\tWelcome ',rsheet.cell_value(main,2))
-                print('(1) Profile\n(2) Edit Profile\n(3) Marks\n(4) Log out')
-                opt = input('\nEnter your option: ')
+                        print('\n------------------------------------------------------------------------------------------\n')
+                        print('\t\t\t\tWelcome ', rsheet.cell_value(main, 2))
+                        print('(1) Profile\n(2) Edit Profile\n(3) Marks\n(4) Log out')
+                        opt = input('\nEnter your option: ')
 
-                if opt == '1':  # Profile
+                        if opt == '1':  # Profile
 
-                    rb = xlrd.open_workbook('studbase.xls')
-                    r_sheet = rb.sheet_by_index(0)
-                    chk = r_sheet.cell_value(main, 19) # it refers to column 19 in excel for all students in that column
-                    #  the default value is False for everyone, because for checking the user first login or
-                    #  more than one times logged in
+                            rb = xlrd.open_workbook('studbase.xls')
+                            r_sheet = rb.sheet_by_index(0)
+                            chk = r_sheet.cell_value(main, 19) # it refers to column 19 in excel for all students in that column
+                            #  the default value is False for everyone, because for checking the user first login or
+                            #  more than one times logged in
 
-                    if chk is False:  # if the 19 coloumn for the user value is False the user log in this account for
-                                    # first time so we have to gather data from the user
+                            if chk == False:  # if the 19 coloumn for the user value is False the user log in this account for
+                                            # first time so we have to gather data from the user
 
-                        print(
-                            '\n------------------------------------------'
-                            '------------------------------------------------\n')
-                        print('\n\t\t\t\tStudent Information\n')
-                        array = []  # Array for store the data entered by the user
-                        attributes = ['Name', 'Exam rollno', 'Gender']
+                                print(
+                                    '\n------------------------------------------'
+                                    '------------------------------------------------\n')
+                                print('\n\t\t\t\tStudent Information\n')
+                                array = []  # Array for store the data entered by the user
+                                attributes = ['Name', 'Exam rollno', 'Gender']
 
-                        for i in range(len(attributes)):
-                            c = input('Enter the ' + attributes[i] + ' :')  # to get the data from the user
-                            array.append(c)  # Adding the user entered data to the array
+                                for i in range(len(attributes)):
+                                    c = input('Enter the ' + attributes[i] + ' :')  # to get the data from the user
+                                    array.append(c)  # Adding the user entered data to the array
 
-                        DOB = input('Enter your Date of Birth(dd-mm-yyyy): ')
-                        array.append(DOB)  # appending the DOB to the array
-                        age = agee(DOB)  # call age function to calculate the age from the DOB
-                        attributes = ['Address', 'City','Postal Code', 'State', 'Nationality',
-                                      'Cell_phone', 'Email', 'Father name', 'Father Occupation', 'Mother Name',
-                                      'Mother Occupation', 'Annual income']
+                                DOB = input('Enter your Date of Birth(dd-mm-yyyy): ')
+                                array.append(DOB)  # appending the DOB to the array
+                                age = agee(DOB)  # call age function to calculate the age from the DOB
+                                attributes = ['Address', 'City','Postal Code', 'State', 'Nationality',
+                                              'Cell_phone', 'Email', 'Father name', 'Father Occupation', 'Mother Name',
+                                              'Mother Occupation', 'Annual income']
 
-                        for i in range(len(attributes)):
-                            c = input('Enter the ' + attributes[i] + ' :')
-                            array.append(c)
+                                for i in range(len(attributes)):
+                                    c = input('Enter the ' + attributes[i] + ' :')
+                                    array.append(c)
 
-                        wob = copy(rb)
-                        w_sheet = wob.get_sheet(0)
+                                wob = copy(rb)
+                                w_sheet = wob.get_sheet(0)
 
-                        for i in range(len(array)):
-                            val = array[i]  # (r,c,value) # to get the user entered data from the array
-                            w_sheet.write(main, i + 2, val)  # and  store to the excel sheet
+                                for i in range(len(array)):
+                                    val = array[i]  # (r,c,value) # to get the user entered data from the array
+                                    w_sheet.write(main, i + 2, val)  # and  store to the excel sheet
 
-                        w_sheet.write(main, 18, age)  # this to store the age in the excel sheet
-                        w_sheet.write(main, 19, 'True')  # After getting the information from the first time signed in
-                        # it change the value in 19 th column to true, so the next time the user signed in the informat-
-                        # ion gathering code is skipped by using the condition sttmt in line 47
-                        w_sheet.write(main, 20,  str(datetime.now()))
-                        wob.save('studbase.xls')  # after storing the values the excel sheet saved
+                                w_sheet.write(main, 18, age)  # this to store the age in the excel sheet
+                                w_sheet.write(main, 19, 'True')  # After getting the information from the first time signed in
+                                # it change the value in 19 th column to true, so the next time the user signed in the informat-
+                                # ion gathering code is skipped by using the condition sttmt in line 47
+                                w_sheet.write(main, 20,  str(datetime.now()))
+                                wob.save('studbase.xls')  # after storing the values the excel sheet saved
+                                pro(user_name,pwd)
 
-                    else:
+                            else:
 
-                        dob = autoage(main)  # to auto calculate the age, the user did not want to change the age
-                        # for every year
-                        rb = xlrd.open_workbook('studbase.xls')
-                        cb = copy(rb)
-                        w_sheet = cb.get_sheet(0)
-                        DOB = dob.split('-')
-                        DOB = list(map(int, DOB))
-                        Birth_year = DOB[2]
-                        current_date = datetime.now()
-                        current_year = current_date.year
-                        age = current_year - Birth_year
-                        w_sheet.write(main, 18, age)
-                        cb.save('studbase.xls')
-                        infor(main)  # to show the profle of the signed in user
+                                dob = autoage(main)  # to auto calculate the age, the user did not want to change the age
+                                # for every year
+                                rb = xlrd.open_workbook('studbase.xls')
+                                cb = copy(rb)
+                                w_sheet = cb.get_sheet(0)
+                                DOB = dob.split('-')
+                                DOB = list(map(int, DOB))
+                                Birth_year = DOB[2]
+                                current_date = datetime.now()
+                                current_year = current_date.year
+                                age = current_year - Birth_year
+                                w_sheet.write(main, 18, age)
+                                cb.save('studbase.xls')
+                                infor(main)  # to show the profle of the signed in user
+                                pro(user_name, pwd)
 
-                elif opt == '2':
-                    print(
-                        '\n------------------------------------------'
-                        '------------------------------------------------\n')
+                        elif opt == '2':
 
-                    print('\n\t\t\t\tProfile Editing\n')
-                    info = ['1, Name', '\t2, Exam Roll', '\t3, Gender', '\n4, DOB', '\t5, Address', '\t6, city',
-                            '\n7, Postal code',
-                            '\t8, State', '\t9, Nationality', '\n10, Phone No.', '\t11, E-mail', '\t12, Father name',
-                            '\n13, Father occupation',
-                            '\t14, Mother Name', '\t15, Mother occupation', '\n16, Annual Income', '\t17, Age']
+                            print(
+                                '\n------------------------------------------'
+                                '------------------------------------------------\n')
 
-                    print(''.join(map(str, info)))  # for printing the list without square brackets and comma
-                    info = ['Name', 'Exam Roll', 'Gender', 'DOB', 'Address', 'city', 'Postal code',
-                            'State', 'Nationality', '10, Phone No.', 'E-mail', 'Father name', 'Father occupation',
-                            'Mother Name', 'Mother occupation', 'Annual Income', ' Age']
-                    e = input('how many options that u want to edit: ')
-                    ed = input('\nEnter the options that you want to edit(with space in numbers): ')  # this get the options in number
-                    edar = ed.split()  # to split the user entered options
-                    edr = list(map(int, edar))  # to convert that splitted words
-                    # print(edr)
-                    rb = xlrd.open_workbook('studbase.xls')
-                    wb = copy(rb)
-                    w_sheet = wb.get_sheet(0)
-                    ar = []  # to store the changed values
-                    # main = 9
+                            print('\n\t\t\t\tProfile Editing\n')
+                            info = ['1, Name', '\t2, Exam Roll', '\t3, Gender', '\n4, DOB', '\t5, Address', '\t6, city',
+                                    '\n7, Postal code',
+                                    '\t8, State', '\t9, Nationality', '\n10, Phone No.', '\t11, E-mail', '\t12, Father name',
+                                    '\n13, Father occupation',
+                                    '\t14, Mother Name', '\t15, Mother occupation', '\n16, Annual Income', '\t17, Age']
 
-                    for i in edr:
-                        edi = input('Enter ' + info[i - 1] + ': ')  # to get the values to change
-                        ar.append(edi)  # append that values in 'ar' list
+                            print(''.join(map(str, info)))  # for printing the list without square brackets and comma
+                            info = ['Name', 'Exam Roll', 'Gender', 'DOB', 'Address', 'city', 'Postal code',
+                                    'State', 'Nationality', '10, Phone No.', 'E-mail', 'Father name', 'Father occupation',
+                                    'Mother Name', 'Mother occupation', 'Annual Income', ' Age']
+                            e = input('how many options that u want to edit: ')
+                            ed = input('\nEnter the options that you want to edit(with space in numbers): ')  # this get the options in number
+                            edar = ed.split()  # to split the user entered options
+                            edr = list(map(int, edar))  # to convert that splitted words
+                            # print(edr)
+                            rb = xlrd.open_workbook('studbase.xls')
+                            wb = copy(rb)
+                            w_sheet = wb.get_sheet(0)
+                            ar = []  # to store the changed values
+                            # main = 9
 
-                    n = 0
-                    for i in edr:
-                        w_sheet.write(main, i + 1, ar[n])
-                        n += 1
+                            for i in edr:
 
-                    wb.save('studbase.xls')
+                                edi = input('Enter ' + info[i - 1] + ': ')  # to get the values to change
+                                ar.append(edi)  # append that values in 'ar' list
 
-                elif opt == '3':
+                            n = 0
 
-                    print('Marks')
+                            for i in edr:
 
-                else:
+                                w_sheet.write(main, i + 1, ar[n])
+                                n += 1
 
-                    iput()
+                            wb.save('studbase.xls')
+                            pro(user_name, pwd)
 
-                found = True
-                break
+                        elif opt == '3':
+
+                            print('Marks')
+                            pro(user_name, pwd)
+
+                        else:
+                            iput()
+                            #break
+            pro(user_name, pwd)
+
+
+
 
             if user_name == 'admin' and str(rsheet.cell_value(1, 0 + 1)):
 
@@ -155,7 +168,7 @@ def iput():
                 print('(1) View Student Profile\n(2) Enter Students Mark\n(3) View Students Marks\n(4) Log out\n')
                 ch = input('Enter the option: ')
 
-                if ch == '1' :
+                if ch == '1':
 
                     loc = ("studbase.xls")
                     wb = xlrd.open_workbook(loc)
